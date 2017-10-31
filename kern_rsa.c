964ab7248db60f99ec4ab1ee2cda59c841592662
abhiram haridas <abhiramharidas@gmail.com>
2017-11-01 01:06:16 +0530

    Trying out non SCSI read/write

--- kern_rsa.c
+++ kern_rsa.c
@@ -1,16 +1,115 @@
-/*  KERNEL LEVEL RSA ENCRYTPTION DRIVER */
+/*  KERNEL LEVEL RSA ENCRYTPTION USB DRIVER */
 
 
-#include <linux/module.h>
-#include <linux/version.h>
 #include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/kref.h>
+#include <linux/uaccess.h>
 #include <linux/usb.h>
+#include <linux/mutex.h>
+
+
+#define MIN(a,b) (((a) <= (b)) ? (a) : (b))
+#define BULK_EP_OUT 0x02
+#define BULK_EP_IN 0x81
+#define MAX_PKT_SIZE 1024
+
+static struct usb_device *device;
+static unsigned char bulk_buf[MAX_PKT_SIZE];                      //static buffer for storing data for read and write 
+
+
+
+
+static int pen_open(struct inode *i, struct file *f)              //returning 0 for open and close
+{
+    return 0;
+}
+static int pen_close(struct inode *i, struct file *f)
+{
+    return 0;
+}
+static ssize_t pen_read(struct file *f, char __user *buf, size_t cnt, loff_t *off)
+{
+    int retval;
+    int read_cnt;
+ 
+    /* Read the data from the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN),
+            bulk_buf, MAX_PKT_SIZE, &read_cnt, 5000);
+    if (retval)
+    {
+        printk(KERN_ERR "Error. Bulk message read returned %d\n", retval);
+        return retval;
+    }
+    if (copy_to_user(buf, bulk_buf, MIN(cnt, read_cnt)))
+    {
+        return -EFAULT;
+    }
+ 
+    return MIN(cnt, read_cnt);
+}
+static ssize_t pen_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off)
+{
+    int retval;
+    int wrote_cnt = MIN(cnt, MAX_PKT_SIZE);
+ 
+    if (copy_from_user(bulk_buf, buf, MIN(cnt, MAX_PKT_SIZE)))
+    {
+        return -EFAULT;
+    }
+ 
+    /* Write the data into the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT),
+            bulk_buf, MIN(cnt, MAX_PKT_SIZE), &wrote_cnt, 5000);
+    if (retval)
+    {
+        printk(KERN_ERR "Error. Bulk message write returned %d\n", retval);
+        return retval;
+    }
+ 
+    return wrote_cnt;
+}
+ 
+static struct file_operations fops =
+{
+    .open = pen_open,
+    .release = pen_close,
+    .read = pen_read,
+    .write = pen_write,
+};
+
+static struct usb_class_driver pen_class = {
+
+	.name = "pen%d",
+	.fops = &fops,
+};
 
 
 static int flash_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
+
+	int retval;
+ 
+    device = interface_to_usbdev(interface);
+
     printk(KERN_INFO "USB flash drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
-    printk(KERN_INFO "");
+    printk(KERN_INFO "");    
+ 
+
+    if ((retval = usb_register_dev(interface, &pen_class)) < 0)                  //registering character interface for device
+    {
+        printk(KERN_ERR "Error. Minor not obtained");
+        printk(KERN_INFO "");
+    }
+    else
+    {
+        printk(KERN_INFO "Minor obtained: %d\n", interface->minor);
+        printk(KERN_INFO "");
+    }
+ 
+    return retval;
 
     return 0;
 }
@@ -19,6 +118,8 @@ static void flash_disconnect(struct usb_interface *interface)
 {
     printk(KERN_INFO "Pen drive removed\n");
     printk(KERN_INFO "");
+
+    usb_deregister_dev(interface,&pen_class);
 }
  
 static struct usb_device_id pen_table[] =
